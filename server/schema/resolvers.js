d864279a6b37e146ee5336e152dae74528e72e4c
Eric-JG <guerrero.eric.j@gmail.com>
2021-09-23 20:24:36 -0400

    weapon and enemy schema

--- server/schema/resolvers.js
+++ server/schema/resolvers.js
@@ -41,35 +41,7 @@ const resolvers = {
     },
   },
 
-  // saveBook: async (parent, args, context) => {
-  //   if (context.user) {
-  //     //   const savedBook = await Book.create({ ...args, username: context.user.username });
 
-  //     const updatedUser = await User.findByIdAndUpdate(
-  //       { _id: context.user._id },
-  //       { $addToSet: { savedBooks: args.input } },
-  //       { new: true },
-  //     );
-
-  //     return updatedUser;
-  //   }
-
-  //   throw new AuthenticationError("You need to be logged in!");
-  // },
-
-  // removeBook: async (parent, args, context) => {
-  //   if (context.user) {
-  //     const updatedUser = await User.findOneAndUpdate(
-  //       { _id: context.user._id },
-  //       { $pull: { savedBooks: { bookId: args.bookId } } },
-  //       { new: true },
-  //     );
-
-  //     return updatedUser;
-  //   }
-
-  //   throw new AuthenticationError("You need to be logged in!");
-  // },
 };
 
 module.exports = resolvers;
--- server/schema/typeDefs.js
+++ server/schema/typeDefs.js
@@ -1,30 +1,28 @@
 const { gql } = require("apollo-server-express");
 
 const typeDefs = gql`
-
   type Query {
     me: User
   }
 
-  type Mutation {
-    login(email: String!, password: String!): Auth
-    addUser(username: String!, email: String!, password: String!): Auth
-  }
-
   type Character {
     _id: ID!
-    Id: String
+    name: String
+    weapons: [Weapons]
+  }
+
+  type Weapons {
+    name: String
+  }
+
+  type Enemy {
+    name: String
+    weapons: [Weapons]
   }
 
   type Auth {
     token: ID!
     user: User
-  }
-
-  type User {
-    _id: ID
-    username: String
-    email: String
   }
 `;
 
